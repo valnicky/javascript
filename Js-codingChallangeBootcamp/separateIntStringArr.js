3625c78a6fcaf148bf8b019fcca84415a1565ef4
valnicky <valery0007@gmail.com>
2021-11-28 15:02:01 +0100

    adding file

--- Js-codingChallangeBootcamp/separateIntStringArr.js
+++ Js-codingChallangeBootcamp/separateIntStringArr.js
@@ -1,8 +1,14 @@
 /**given s string or an  array write a function that takes a string or an array return integers from the strings/array */
 
 function separate(strnum) {
+    let newstr = [].concat(strnum).join('');
 
+    let result = newstr.match(/\d+/g).join('');
+    return result;
 }
+
+//if(typeof(strnum) === 'string'){}else if(typeof(strnum) === 'object'){}
+
 
 separate('he5llo1234'); //51234
 separate(['h', 2, 'e', 1, 'o', 3, 4]); //2134
